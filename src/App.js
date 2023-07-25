3cee50affde8bda38b7429e8d63544ef48b4fdee
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2023-07-25 12:40:37 +0530

    components & Props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 
+//This is a custom component 
+const Person =({name,age,gender})=>{
+return(
+  <>
+  <h1>Name: {name}</h1>
+  <h2>Age:{age}</h2>
+  <h2>Gender:{gender}</h2>
+  </>
+)
+}
+
 const App = () => {
-  const name='Ram';
-  // const isName=true;
+  
   return (
     <div>
-      Hello
-      {
-        name?(<>
-        <h1>Ram Mohan Patel</h1>
-        </>):(
-          <>
-          No name is there
-          </>
-        )
-      }
+       <Person name={'Dev'} age={18} gender={'Male'}  />
+       <Person name={'Harry'} age={13} gender={'Male'}/>
+       <Person name={'Devansh'} age={14} gender={'Male'}/>
+       <Person name={'Devesh'} age={16} gender={'Male'}/>
+       <Person name={'Don'} age={12} gender={'Male'}/>
     </div>
   )
 }
